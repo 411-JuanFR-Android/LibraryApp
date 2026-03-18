d8e55ea8eca0e9ca6f872bfe54c63e0c6c14dbe0
Juan Frausto <juanfr97@hotmail.com>
2026-03-18 16:12:55 -0600

    Initial commit

---
+++ app/src/main/java/com/pjasoft/libraryapp/screens/BooksScreen.kt
@@ -0,0 +1,63 @@
+package com.pjasoft.libraryapp.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import com.pjasoft.libraryapp.components.Header
+import com.pjasoft.libraryapp.ui.theme.LibraryAppTheme
+
+@Composable
+fun BooksScreen(
+    innerPadding : PaddingValues
+){
+    val colors = MaterialTheme.colorScheme
+    val typography = MaterialTheme.typography
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colors.background)
+    ) {
+        Header(
+            innerPadding
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun BooksScreenPreview(){
+    LibraryAppTheme {
+        BooksScreen(
+            innerPadding = PaddingValues(15.dp)
+        )
+    }
+}
